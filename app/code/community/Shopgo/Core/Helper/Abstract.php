af337d153cefc1eb2ad5638bc30b66f92b507246
am-shopgo <ammar@shopgo.me>
2015-02-16 09:52:45 +0200

    Changed helper class definition.

--- app/code/community/Shopgo/Core/Helper/Abstract.php
+++ app/code/community/Shopgo/Core/Helper/Abstract.php
@@ -23,7 +23,7 @@
  * @package     Shopgo_Core
  * @author      Ammar <ammar@shopgo.me>
  */
-abstract Shopgo_Core_Helper_Abstract extends Mage_Core_Helper_Abstract
+class Shopgo_Core_Helper_Abstract extends Mage_Core_Helper_Abstract
 {
     /**
      * Log file name
